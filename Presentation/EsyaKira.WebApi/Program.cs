a50f8adb4d989fc6ae47a7a0687c8b407f3644ff
Oguzhan <oguzhanbilalk@gmail.com>
2024-06-10 21:55:17 +0300

    completed madiator

--- Presentation/EsyaKira.WebApi/Program.cs
+++ Presentation/EsyaKira.WebApi/Program.cs
@@ -1,20 +1,67 @@
 using EsyaKira.Application.Features.CQRS.Handlers.AboutHandlers;
-using EsyaKira.Application.Features.CQRS.Queries.AboutQueries;
+using EsyaKira.Application.Features.CQRS.Handlers.BannerHandlers;
+using EsyaKira.Application.Features.CQRS.Handlers.BrandHandlers;
+using EsyaKira.Application.Features.CQRS.Handlers.CategoryHandlers;
+using EsyaKira.Application.Features.CQRS.Handlers.ContactHandlers;
+using EsyaKira.Application.Features.CQRS.Handlers.ProductHandlers;
+
 using EsyaKira.Application.Interfaces;
+
+using EsyaKira.Application.Interfaces.ProductInterfaces;
+using EsyaKira.Application.Services;
 using EsyaKira.Persistance.Context;
 using EsyaKira.Persistance.Repositories;
+using EsyaKira.Persistance.Repositories.ProductRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<EsyaKiraContext>();
 builder.Services.AddScoped(typeof(IRepository<>),(typeof(Repository<>)));
+builder.Services.AddScoped(typeof(IProductRepository), typeof(ProductRepository));
+
 
 builder.Services.AddScoped<GetAboutQueryHandler>();
 builder.Services.AddScoped<GetAboutByIdQueryHandler>();
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
+
+builder.Services.AddScoped<GetProductQueryHandler>();
+builder.Services.AddScoped<GetProductByIdQueryHandler>();
+builder.Services.AddScoped<CreateProductCommandHandler>();
+builder.Services.AddScoped<UpdateProductCommandHandler>();
+builder.Services.AddScoped<RemoveProductCommandHandler>();
+builder.Services.AddScoped<GetProductWithBrandQueryHandler>();
+builder.Services.AddScoped<GetLast5ProductsWithBrandQueryHandler>();
+
+builder.Services.AddScoped<GetCategoryQueryHandler>();
+builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+
+builder.Services.AddScoped<GetContactQueryHandler>();
+builder.Services.AddScoped<GetContactByIdQueryHandler>();
+builder.Services.AddScoped<CreateContactCommandHandler>();
+builder.Services.AddScoped<UpdateContactCommandHandler>();
+builder.Services.AddScoped<RemoveContactCommandHandler>();
+
+builder.Services.AddApplicationService(builder.Configuration);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
